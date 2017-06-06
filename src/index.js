cd773399f46034f96c29623e59c86c1eea4e62c4
lol-russo <jack.lorusso@digivizer.com>
2017-06-07 00:26:19 +1000

    Set up css-modules and sass with webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import HelloWorld from './components/hello-world.jsx';
+import styles from './styles/base.scss' 
 
 ReactDOM.render(<HelloWorld />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,6 @@
 const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -13,11 +8,35 @@ module.exports = {
     path: path.resolve('dist'),
     filename: 'bundle.js'
   },
+
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader?modules&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
+        }),
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader?modules&importLoader=2&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader'
+        }),
+      },
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [
+    new ExtractTextPlugin("style.css"),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
 }
